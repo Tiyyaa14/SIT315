a6e2b4ad3bedcf4a9360ad73a06210b141c34d9d
Tiyyaa14 <tiyakukar05@gmail.com>
2025-02-12 11:44:04 +0530

    Create Task1.2.ino

---
+++ Module1/Task1.2.ino
@@ -0,0 +1,25 @@
+const int pirPin = 7;   
+const int ledPin = 13;  
+volatile bool motionDetected = false;  
+
+void motionISR() {
+    motionDetected = true;  
+}
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(pirPin, INPUT);
+    pinMode(ledPin, OUTPUT);
+    
+    attachInterrupt(digitalPinToInterrupt(pirPin), motionISR, RISING);
+}
+
+void loop() {
+    if (motionDetected) {
+        Serial.println("Motion detected!");
+        digitalWrite(ledPin, HIGH);
+        delay(2000);  
+        digitalWrite(ledPin, LOW);
+        motionDetected = false;  
+    }
+}
